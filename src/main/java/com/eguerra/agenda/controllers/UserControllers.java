d334b3abc4095fff1cd6a56673873c6a05f6035a
Edfram-Guerra <edfram.guerra@gmail.com>
2024-09-11 00:35:34 -0300

    Implementado os controladores de usuario

--- src/main/java/com/eguerra/agenda/controllers/UserControllers.java
+++ src/main/java/com/eguerra/agenda/controllers/UserControllers.java
@@ -8,6 +8,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.Stack;
+
 @RestController
 @RequestMapping("/agenda")
 public class UserControllers {
@@ -29,5 +33,59 @@ public class UserControllers {
     }
 
     @GetMapping("/userList")
+    public ResponseEntity<List<User>>listUser(){
+        try {
+            List<User>userList=userService.getAllusers();
+            return ResponseEntity.ok(userList);
+        }catch (Exception e){
+            return  ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
 
+    @GetMapping("/userList/{id}")
+    public ResponseEntity<Object> listOneUser(@PathVariable(value = "id") Long id) {
+        try {
+
+            Optional<User> user = Optional.ofNullable(userService.getUserById(id));
+
+            if (user.isPresent()) {
+                return ResponseEntity.ok(user.get());
+            } else {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found");
+            }
+        } catch (Exception e) {
+
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An error occurred while processing the request.");
+        }
+    }
+
+    @GetMapping("/userName")
+    public ResponseEntity<List<User>> findUsers(@RequestParam String name){
+        List<User> users = userService.findUserByName(name);
+        if(users.isEmpty()){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }else {
+            return  new ResponseEntity<>(users,HttpStatus.OK);
+        }
+    }
+
+    @PutMapping("/userUpdate/{id}")
+    public ResponseEntity<Object>updateUser(@PathVariable(value = "id")Long id, @RequestBody @Valid UserDto userDto){
+        try {
+            Optional<User>optionalUser = Optional.ofNullable(userService.updateUser(id, userDto));
+            return ResponseEntity.ok("User updated successfully");
+        }catch (Exception e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Unable to update user");
+        }
+    }
+
+    @DeleteMapping("/userDelete/{id}")
+    public ResponseEntity<Object>deleteUser(@PathVariable(value = "id") Long id){
+        try {
+            Optional<User>optionalUser=userService.deleteUser(id);
+            return ResponseEntity.ok("User successfully deleted");
+        }catch (Exception e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error when deleting the person");
+        }
+    }
 }
